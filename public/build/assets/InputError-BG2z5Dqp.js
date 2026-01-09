9b93e64c24fe24038cf0a8be0ce3393d8674be2a
Hassan Jamal <hassanjamal8735@gmail.com>
2026-01-10 00:14:34 +0500

    Build Created

---
+++ public/build/assets/InputError-BG2z5Dqp.js
@@ -0,0 +1,1 @@
+import{j as x}from"./app-DAKRhxGg.js";function o({message:r,className:t="",...n}){return r?x.jsx("p",{...n,className:"text-xs text-red-500 ml-4 "+t,children:r}):null}export{o as I};
--- public/build/assets/useTranslation-Hg9Scd8p.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./app-9sX3B-Kc.js";function a(){const{translations:n}=r().props;return{t:t=>n[t]||t}}export{a as u};
